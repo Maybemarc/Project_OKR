a66be02abac977e784717e037ab9bef213a6f8e8
Maybemarc <marcspectre777@gmail.com>
2025-05-30 17:25:06 +0530

    fixes

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
+  "https://project-okr.vercel.app"
   "http://localhost:5173"
 ];
 app.use(express.json());
