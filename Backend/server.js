ea2d03b39e97981a4d5a38766987cd3b68145c43
Maybemarc <marcspectre777@gmail.com>
2025-05-30 15:55:15 +0530

    changed

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({ origin: ['https://your-frontend.vercel.app', 'http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['https://project-okr.vercel.app', 'http://localhost:5173'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
