4a4f0951a13565769e26052a5b8df4d6079c90c7
Maybemarc <marcspectre777@gmail.com>
2025-05-30 16:44:13 +0530

    fixes

--- Frontend/my-okr_project/vite.config.js
+++ Frontend/my-okr_project/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
   plugins: [react()],
 })
